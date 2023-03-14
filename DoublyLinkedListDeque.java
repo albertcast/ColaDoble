8ede91fb8df99361c3686fabd6debd1d1b02bbb9
albertcast <0619700846@uma.es>
2023-03-14 12:31:18 +0100

    Parte get y sort

---
+++ DoublyLinkedListDeque.java
@@ -0,0 +1,139 @@
+package org.mps.deque;
+
+import java.util.Comparator;
+
+public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
+
+    private DequeNode<T> first;
+    private DequeNode<T> last;
+    private int size;
+
+    public DoublyLinkedListDeque() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public void prepend(T value) {
+    	if(size == 0) {
+    		DequeNode<T> aux = new DequeNode<T>(value, null, null);
+    		first = aux;
+    		last = aux;
+    		size++;
+    	} else {
+    		DequeNode<T> aux = new DequeNode<T>(value, null, first);
+    		first.setPrevious(first);
+    		first = aux;
+    		size++;
+    	}  	
+    }
+
+    public void append(T value) {
+    	if(size == 0) {
+    		DequeNode<T> aux = new DequeNode<T>(value, null, null);
+    		first = aux;
+    		last = aux;
+    		size++;
+    	} else {
+    		DequeNode<T> aux = new DequeNode<T>(value, last, null);
+    		last.setNext(aux);
+    		last = aux;
+    		size++;
+    	} 
+    }
+
+    public void deleteFirst() {
+    	if(size > 1) {
+	    	first = first.getNext();
+	    	size--;
+    	} else if (size == 1) {
+    		first = null;
+    		last = null;
+    		size--;
+    	} else {
+    		throw new DoubleEndedQueueException("Se han intentado eliminar un elemento de una cola vacía");
+    	}
+    }
+
+    public void deleteLast() {
+    	if(size > 1) {
+	    	last = last.getPrevious();
+	    	size--;
+    	} else if (size == 1) {
+    		first = null;
+    		last = null;
+    		size--;
+    	} else {
+    		throw new DoubleEndedQueueException("Se han intentado eliminar un elemento de una cola vacía");
+    	}
+    }
+
+    public T first() {
+    	if(first != null) {
+    		return first.getItem();
+    	} else {
+    		return null;
+    	}
+    }
+
+    public T last() {
+    	if(last != null) {
+    		return last.getItem();
+    	} else {
+    		return null;
+    	}
+    }
+
+    public int size() {
+        return size;
+    }
+
+	@Override
+	public T get(int index) {
+		if(size == 0 || index < 0 || index > size){
+			throw new IndexOutOfBoundsException("El indice buscado no está contenido en la lista (indice < 0 o indice > tamaño de la lista. Index = " + index);
+		}
+
+		int i = 0;
+		DequeNode<T> aux = first;
+		while(i < index){
+			aux = aux.getNext();
+			i++;
+		}
+
+
+
+
+
+		return aux.getItem();
+	}
+
+	@Override
+	public boolean contains(T value) {
+		return false;
+	}
+
+	@Override
+	public void remove(T value) {
+
+	}
+
+	@Override
+	public void sort(Comparator<? super T> comparator) {
+		if(size > 1) {
+			DequeNode<T> auxFirst = first;
+			for (int i = 0; i < size; i++) {
+				DequeNode<T> auxSecond = auxFirst.getNext();
+				for (int j = i; j < size-1; j++) {
+					if (auxSecond != null && comparator.compare(auxFirst.getItem(), auxSecond.getItem()) > 0) {
+						T placeholder = auxFirst.getItem();
+						auxFirst.setItem(auxSecond.getItem());
+						auxSecond.setItem(placeholder);
+					}
+					auxSecond = auxSecond.getNext();
+				}
+				auxFirst = auxFirst.getNext();
+			}
+		}
+	}
+}
